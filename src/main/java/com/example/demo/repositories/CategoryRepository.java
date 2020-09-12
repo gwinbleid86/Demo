dd90c58f0a455f9204b42e3b394564e12a95dc1b
Podstavin Aleksei <gwinbleid86@gmail.com>
2020-09-12 13:46:55 +0600

    add repositories

---
+++ src/main/java/com/example/demo/repositories/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CategoryRepository extends PagingAndSortingRepository {
+}
