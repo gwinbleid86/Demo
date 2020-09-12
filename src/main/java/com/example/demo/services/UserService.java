869076bca644f089d24b71239cfcd90c01e4fc48
Podstavin Aleksei <gwinbleid86@gmail.com>
2020-09-12 13:49:02 +0600

    add services

---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,11 @@
+package com.example.demo.services;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserService {
+
+}
